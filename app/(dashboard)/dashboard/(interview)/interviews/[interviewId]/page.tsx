d22b8fa96ff782803eaebd91402cb89e1e38b017
Beiryu <dinhnguyenkhanh210401@gmail.com>
2024-12-31 08:08:22 +0700

    feat: integrate mail management system with new components and state management; add Mail, MailList, MailDisplay, and AccountSwitcher components, implement Jotai for state management, and enhance UI with resizable panels for better user experience

--- app/(dashboard)/dashboard/(interview)/interviews/[interviewId]/page.tsx
+++ app/(dashboard)/dashboard/(interview)/interviews/[interviewId]/page.tsx
@@ -1,11 +1,28 @@
+import { cookies } from "next/headers"
+import { accounts, mails } from "@/mocks/data"
+
 import History from "@/components/history"
 import { LiveInterviewPlayground } from "@/components/live-interview-playground"
+import { Mail } from "@/components/mail"
 
 export default function LiveInterviewDetailPage() {
+  const layout = cookies().get("react-resizable-panels:layout:mail")
+  const collapsed = cookies().get("react-resizable-panels:collapsed")
+
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined
+  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined
+
   return (
     <div className="flex flex-col gap-12">
       <LiveInterviewPlayground />
       <History />
+      <Mail
+        accounts={accounts}
+        mails={mails}
+        defaultLayout={defaultLayout}
+        defaultCollapsed={defaultCollapsed}
+        navCollapsedSize={4}
+      />
     </div>
   )
 }
